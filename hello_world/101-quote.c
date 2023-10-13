b767c2553e8506f7bcf1a7314d48182342fd1a80
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-13 07:46:06 -0700

    exit status one

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Using the main function as entry point for execution
+ * Creating a program with an exit status of one
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	!printf;
+	return (0);
+}
