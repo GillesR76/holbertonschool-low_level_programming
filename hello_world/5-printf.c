af9ee75a46b750925a7327d91e75373198cc0e84
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-13 04:16:12 -0700

    using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Using the main function as entry point for execution
+ * Using the printf() function to print
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
