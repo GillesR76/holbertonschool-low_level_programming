55b2c4b4cffacc84e508a736bb7ec1b4ece4ee6a
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-13 02:52:34 -0700

    update function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
+	/**
+	 *The code below will create a string and print it using puts
+	 */
+
 	char string1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string1);
