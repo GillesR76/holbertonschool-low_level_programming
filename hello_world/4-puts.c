6fd529b2136ad6d1d2cc251eb3ea3692806eeff4
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-13 02:30:33 -0700

    update function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	char string1[]="\"Programming is like building a multilingual puzzle";
+	char string1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string1);
 
