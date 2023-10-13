f42535eec8c848255ba6f9e034b52fb1340e3fb0
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-13 03:03:15 -0700

    update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(void)
+	/**
+	 * Using the main function as the entry point of the program i.e where the 
+	 * execution of the program begins
+	 */
 {
 	/**
-	 *The code below will create a string and print it using puts
+	 * The code below will create a string and print it using puts
 	 */
 
 	char string1[] = "\"Programming is like building a multilingual puzzle";
