e4071f0035591dcce68f7b93ef1197abdcf3031d
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-16 06:13:35 -0700

    using while loop twice

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print the alphabet in lowercase and then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char alphabet1 = 'a';
+	char alphabet2 = 'A';
+
+        while (alphabet1 <= 'z')
+{
+        putchar(alphabet1);
+        alphabet1++;
+}
+
+	while (alphabet2 <= 'Z')
+{
+	putchar(alphabet2);
+	alphabet2++;
+}
+        putchar('\n');
+
+        return (0);
+}
