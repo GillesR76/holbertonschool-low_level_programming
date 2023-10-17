2ec89bb1be3ff0e6b8d1e2972de2b457b4037515
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-17 07:14:44 -0700

    update

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int_islower - check for lowercase character
+ * @int c: test for lowercase letter
+ *
+ * Return (0)
+*/
+
+int _islower(c)
+{
+	char ch = 'c';
+	_islower(ch);
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int _putchar(char c);
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
