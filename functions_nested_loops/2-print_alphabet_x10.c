36a7f2894c7cfa427cfd41b3aaba5dbc07ff30d0
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-17 06:40:19 -0700

    print alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * @void: void
+ *
+ * Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	char al2;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (al2 = 'a'; al2 <= 'z'; al2++)
+		{
+			_putchar(al2);
+		}
+
+	_putchar('\n');
+	}
+}
