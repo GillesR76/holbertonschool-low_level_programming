b250506dee93944fb0a16a025bb040005bbe9bd4
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-19 06:11:57 -0700

    update

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,33 +9,31 @@
 
 void times_table(void)
 {
-	int x;
-	int y;
+     int x, y;
 
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
-		{
-			if ((x * y) < 10)
-			{
-				_putchar((x * y) + '0');
-				if (y < 9)
-					_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(((x * y) / 10) + '0');
-				_putchar(((x * y) % 10) + '0');
-				if (y < 9)
-					_putchar(',');
-				_putchar(' ');
-			}
+     for (x = 0; x <= 9; x++)
+     {
+         _putchar('0');
+         for (y = 1; y <= 9; y++)
+         {
+             int product = x *  y;
 
+                 if (product < 10)
+             {
+                 _putchar(',');
+                 _putchar(' ');
+                 _putchar(' ');
+                 _putchar(product + '0');
 
-
-		}
-		_putchar('\n');
-	}
-}
+             }
+             else
+             {
+                 _putchar(',');
+                 _putchar(' ');
+                 _putchar((product / 10) + '0');
+                 _putchar((product % 10) + '0');
+             }
+                 }
+         _putchar('\n');
+     }
+ }
