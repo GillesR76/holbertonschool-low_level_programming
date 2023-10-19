ee3dda9437c1e7a116f2451a02b429db84ba9550
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-19 06:28:15 -0700

    add two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add tow integers and return the result
+ * @int, int: two integers
+ *
+ * Return: result
+ */
+
+int add(int x, int y)
+{
+	int sum;
+	sum = x + y;
+
+	return (sum);
+
+}
+
