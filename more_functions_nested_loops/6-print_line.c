1931d14a97d9f65302a6439661ac4aedaeb3a273
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-20 08:25:42 -0700

    draw a straight line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,12 @@ void print_line(int n)
 {
 	int t;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-
-	for (t = 1; t <= n ; t++)
-	{
+		for (t = 1; t <= n; t++)
+		{
 			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
