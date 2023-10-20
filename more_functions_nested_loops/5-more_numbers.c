dcae45178b6306b970cb54d3a806663009ecd0da
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-20 07:43:37 -0700

    draw a straight line

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,7 @@ void more_numbers(void)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if ((y / 10) > 0)
+			if (y > 9)
 			{
 				_putchar((y / 10) + '0');
 			}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -15,3 +15,5 @@ void print_numbers(void);
 void print_most_numbers(void);
 
 void more_numbers(void);
+
+void print_line(int n);
