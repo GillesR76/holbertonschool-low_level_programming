da867f77ecf88551a0017c3dfa7b3641fe7c1568
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-20 06:59:09 -0700

    create a chart of ten times the numbers from zero to forteen

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers from 0 to 14
+ * @void: void
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if ((y / 10) > 0)
+			{
+				_putchar((y / 10) + '0');
+			}
+		_putchar((y % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
