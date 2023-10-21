8c0d278db066b0ba9d7ba5af961a6c1f87a7da77
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-21 07:24:02 -0700

    update

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -14,14 +14,14 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y < x; y++)
 			{
 				_putchar(' ');
 			}
-		_putchar('\\');
-		_putchar('\n');
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 	else
