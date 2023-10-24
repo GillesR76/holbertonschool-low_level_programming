bd5944e90e910d629efa1d0bfbde708589d5b4f4
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-24 09:19:13 -0700

    update

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,12 @@ void print_array(int *a, int n)
 {
 	int i;
 	
-		for (i = 0; i <= n - 1; i++)
+		for (i = 0; i < n; i++)
 		{
 			printf("%d", a[i]);
 			if (i < n - 1)
 				printf("%d, ", a[i]);
 		}
+
 	printf("\n");
 }
