e05bf886dc9b937c19e5231ce9971b8e31627d2e
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-25 03:05:51 -0700

    copy a string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies a string
+ * @dest: destination character array where the content is to be copied
+ * @src: source character array which is to be copied
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (src[x])
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	return (dest);
+
+}
