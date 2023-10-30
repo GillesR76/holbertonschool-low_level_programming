9e926ced6e69e9f6b0afab461974f9286e3824da
Gilles Richard <gilles-richard76@hotmail.com>
2023-10-30 06:34:33 -0700

    update

---
+++ recursion/main.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
