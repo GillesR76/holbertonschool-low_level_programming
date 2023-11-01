7996c4629a37a6d91583fe523ffaa5f63cf7ca57
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-01 04:49:02 -0700

    update

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: (0)
+ */
+
 int main(int argc, char *argv[])
 {
 	argc = 1;
