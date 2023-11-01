1aa582b361f586a2a089551cee01a10d568db277
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-01 04:41:48 -0700

    program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	argc = 1;
+
+	if (argc)
+	{
+		printf("%s\n", *argv);
+	}
+
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
