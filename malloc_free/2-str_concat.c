bcac4a3668a21954f570859eef3babc890222e0b
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-06 06:52:21 -0800

    concatenate with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,11 +19,12 @@ char *str_concat(char *s1, char *s2)
 		s1 = ("");
 	if (s2 == NULL)
 		s2 = ("");
-	while (s1[len1])
+
+	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-	while (s2[len2])
+	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
