899062c545b22eeafa82dd9be961adf1efd15f76
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-08 02:44:54 -0800

    allocate memory with malloc refering to calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,20 +10,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ar;
+	char *ar;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ar = malloc(nmemb * sizeof(size));
+	ar = malloc(nmemb * sizeof(int));
 
 	if (ar == NULL)
 		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
 	{
-		ar[i] = size;
+		ar[i] = 0;
 	}
 
 	return (ar);
