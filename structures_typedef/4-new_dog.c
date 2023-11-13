4809c33f89485e8357cb0f215b097749b4970c83
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-13 06:25:30 -0800

    create a new structure using malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,16 +12,24 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = NULL;
+	int len1 = 0, len2 = 0;
 
-	new_dog = malloc(sizeof(dog_t));
+	while (name[len1] != '\0')
+		len1++;
+	while (owner[len2] != '\0')
+		len2++;
+
+	new_dog = malloc((len1 + len2) * sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
+
+	name = malloc(len1 * sizeof(char));
+	owner = malloc(len2 * sizeof(char));
 
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
 
 	return (new_dog);
-	free(new_dog);
 }
