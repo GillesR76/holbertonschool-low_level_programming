8844fea5fb89f67e619715023bd2938dfb463388
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-13 04:39:33 -0800

    print a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -27,7 +27,7 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
+		printf("Age: %1f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
 }
