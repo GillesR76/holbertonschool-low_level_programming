47c2386cc34ba25e63e458ac9f9b119869c35ccd
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-13 01:44:16 -0800

    update task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct _dog - Structure named dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Second member
+ *
+ */
+
 struct dog {
 	char *name;
 	float age;
