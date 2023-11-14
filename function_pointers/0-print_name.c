dd12a14c588dd7690ca5dd7277a2a6f4d1283385
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-14 05:17:03 -0800

    print a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: char
+ * @f: function pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	
+	f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
