e7698f7ba1184194fbee27c31f6d79c42b8cfce3
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-14 05:27:33 -0800

    print a name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -10,7 +10,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL || f == NULL)
+		return;
 
 	f(name);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+
+#endif
