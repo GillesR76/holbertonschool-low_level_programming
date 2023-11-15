0b6bc67b84c6867032d93799b12de139d4d91936
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-15 07:16:55 -0800

    update task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,10 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ptr, n);
 
+	if (n == 0)
+		return (0);
+
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-			return (0);
 		sum += va_arg(ptr, int);
 	}
 	va_end(ptr);
