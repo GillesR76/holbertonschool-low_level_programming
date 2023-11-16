a722bc0d1950dc7f36fe41002bc6b914217d76e5
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-16 04:51:39 -0800

    print a number

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: char
+ * @n: unsigned int
+ *
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list ptr;
+
+	if (separator == NULL)
+		return;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ptr, unsigned int));
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
