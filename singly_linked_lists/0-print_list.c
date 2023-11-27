c466e7f74f677b09d050bbfa93b02bd9b456eba2
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-27 07:30:20 -0800

    function that prints the elements of a list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 			printf("[0](nil)\n");
 
 		else
-			printf("[%d] %s\n", temp->len, temp->str);
+			printf("[%u] %s\n", temp->len, temp->str);
 
 		temp = temp->next;
 		len++;
