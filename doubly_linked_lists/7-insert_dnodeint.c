5fcdd465f87d7b35a1d5147fa45c5e1a73af863d
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-30 05:17:54 -0800

    add a new node inside a list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,6 +48,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (temp->next != NULL)
 		temp->next->prev = newnode;
 	temp->next = newnode;
-	
+
 	return (newnode);
 }
