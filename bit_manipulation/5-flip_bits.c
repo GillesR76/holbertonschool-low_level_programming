705494e9ab1ac9d9685dcb129944869de209557a
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-01 04:01:37 -0800

    return the number of bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits
+ * @n: unsigned long int
+ * @m: unsigned long int
+ *
+ * Return: the number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result = n ^m;
+	unsigned int count = 0;
+
+	while (result > 0)
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+	return count;
+}
