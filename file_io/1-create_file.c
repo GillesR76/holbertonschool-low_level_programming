b5d067c94c963532ad6d5e4511688b1086c9a775
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-04 13:42:03 -0800

    update

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,28 +20,28 @@ int create_file(const char *filename, char *text_content)
 	while (text_content != NULL && text_content[len] != '\0')
 		len++;
 
-	if (text_content != NULL)
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	buffer = malloc(len + 1);
+	if (buffer == NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		if (fd == -1)
-			return (-1);
+		close(fd);
+		return (-1);
+	}
+	for (i = 0; i < len; i++)
+		buffer[i] = text_content[i];
 
-		buffer = malloc(len + 1);
-		if (buffer == NULL)
-		{
-			close(fd);
-			return (-1);
-		}
-		for (i = 0; i < len; i++)
-			buffer[i] = text_content[i];
-
-		write_bytes = write(fd, buffer, len);
-		if (write_bytes == -1)
-		{
-			free(buffer);
-			close(fd);
-			return (-1);
-		}
+	write_bytes = write(fd, buffer, len);
+	if (write_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (-1);
 	}
 	free(buffer);
 	close(fd);
