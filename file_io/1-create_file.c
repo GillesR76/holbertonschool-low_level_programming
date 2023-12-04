57a9a8e983a14d10d86848895735d3f343533f62
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-04 12:17:47 -0800

    adding changes

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,35 +10,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0, i;
 	char *buffer;
-	ssize_t read_bytes, write_bytes;
+	ssize_t write_bytes;
 
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content != NULL)
+	while (text_content != NULL && text_content[len] != '\0')
+		len++;
+
+	if (text_content != NULL)
 	{
 		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
 
-		buffer = malloc(strlen(text_content) + 1);
+		buffer = malloc(len + 1);
 		if (buffer == NULL)
 		{
 			close(fd);
 			return (-1);
 		}
+		for (i = 0; i < len; i++)
+			buffer[i] = text_content[i];
 
-		read_bytes = read(fd, buffer, *text_content);
-		if (read_bytes == -1)
-		{
-			free(buffer);
-			close(fd);
-			return (-1);
-		}
-
-		write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+		write_bytes = write(fd, buffer, len);
 		if (write_bytes == -1)
 		{
 			free(buffer);
