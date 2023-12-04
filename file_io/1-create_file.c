8c6c49fd92db077dec6fcb3324436d657bdb2b66
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-04 12:22:16 -0800

    adding changes

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		for (i = 0; i < len; i++)
-		{	
+		{
 			buffer[i] = text_content[i];
 			buffer[len] = '\0';
 		}
