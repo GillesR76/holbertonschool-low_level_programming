c25dc5e151cb1f69de9274934b2a972b1bbd4c41
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-05 04:39:51 -0800

    copy the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of strings i.e the arguments
+ *
+ * Return: 0 if success, exit codes if failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	char buffer[1024];
+	int read_bytes, write_bytes;
+
+	if (argc != 3)
+	{
+		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (fd_to == -1)
+		return (-1);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	}
+
+	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes == -1)
+		{
+			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+	if (read_bytes == -1)
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
