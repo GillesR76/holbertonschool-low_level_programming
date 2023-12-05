b1f268d897951ad5d77706cf7536fe9d546aaeef
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-05 00:24:35 -0800

    append text to a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	while (text_content[len] != '\0')
 		len++;
 
-	fd = open(filename, O_APPEND, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
 
