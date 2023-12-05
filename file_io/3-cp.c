399bae8dd98c2d84fac7ea78da000276a4d9ff2a
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-05 04:52:09 -0800

    copy the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,39 +14,45 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	char buffer[1024];
-	int read_bytes, write_bytes;
+	ssize_t read_bytes, write_bytes;
 
 	if (argc != 3)
 	{
-		exit(97);
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
 	}
-
-	fd_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		return (-1);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
 		if (write_bytes == -1)
 		{
-			exit(99);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if (read_bytes == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+
 	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	return (0);
 }
