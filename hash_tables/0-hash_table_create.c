ba0dbcb370a99d05cf3613a433c9d79d69a3903a
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-06 03:02:02 -0800

    update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -27,7 +27,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_hash_table->size = size;
 
 		for (i = 0; i < size; i++)
-			new_hash_table->array = NULL;
+			new_hash_table->array[i] = NULL;
 
 		return (new_hash_table);
 	}
