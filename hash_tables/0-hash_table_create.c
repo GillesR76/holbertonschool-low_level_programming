d62cf8828f233fb69d3a0a65e304e3b09d248b87
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-06 02:16:49 -0800

    update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,10 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
 		return (NULL);
+
+	new_hash_table->array = malloc(sizeof(new_hash_table) * size);
+	if (new_hash_table->array == NULL)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+
+	new_hash_table->size = size;
 
 	return (new_hash_table);
 }
