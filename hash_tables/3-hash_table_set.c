6f4a8759e3054c9ec8f810ba29bcfe1b6fbb9379
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-07 07:26:56 -0800

    add an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,36 +16,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
-	{
-		newnode = malloc(sizeof(hash_node_t));
-		if (newnode == NULL)
-			return (0);
-		newnode->key = strdup(key);
-		newnode->value = strdup(value);
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
 
-		if (newnode->value == NULL)
-		{
-			free(newnode);
-			return (0);
-		}
-		newnode->next = ht->array[index];
-		ht->array[index] = newnode;
-	}
-	else
+	if (ht->array[index] != NULL)
 	{
 		temp = ht->array[index];
 
-		while (temp)
+		while (temp != NULL)
 		{
 			if (strcmp(temp->key, key) == 0)
 			{
 				temp->value = strdup(value);
-				free(temp);
+				free(temp->value);
 				return (1);
 			}
 			temp = temp->next;
 		}
 	}
+	newnode = malloc(sizeof(hash_node_t));
+
+	if (newnode == NULL)
+		return (0);
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	newnode->next = NULL;
+
+	if (newnode->value == NULL)
+	{
+		free(newnode);
+		return (0);
+	}
+	newnode->next = ht->array[index];
+	ht->array[index] = newnode;
+
 	return (1);
 }
