105e260c115fb522fe2233820288faa9d3e64624
GillesR76 <gilles-richard76@hotmail.com>
2024-01-16 14:50:01 +0100

    removing space before colon

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		mid = start + ((end - start) / 2);
 
-		printf("Searching in array : ");
+		printf("Searching in array: ");
 		for (i = start; i <= end; i++)
 		{
 			printf("%d", array[i]);
