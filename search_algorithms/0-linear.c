7f90c13264bed891b0d5c6404d05f38ee87df7cc
GillesR76 <gilles-richard76@hotmail.com>
2024-01-16 13:19:22 +0100

    betty check

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 
 /**
  * linear_search - function that searches for a value in an array of int
- * @array: pointer to the first element of the array 
+ * @array: pointer to the first element of the array
  * @size: number of elements in array
  * @value: value to search for
  * Return: the index of the value searched for
